f2ce12ca5c29cab8c9096c5a895259ad89d77e67
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-30 17:52:59 -0300

    Initial commit.

---
+++ src/com/hstefan/aunctiondroid/AunctionDroidActivity.java
@@ -0,0 +1,13 @@
+package com.hstefan.aunctiondroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AunctionDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
