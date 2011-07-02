1881d279b73e7db61983beeb137a390895082d8a
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-07-02 02:47:32 -0300

    Layouts that will be used by our activities.

--- gen/com/hstefan/aunctiondroid/R.java
+++ gen/com/hstefan/aunctiondroid/R.java
@@ -14,15 +14,36 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int email_text=0x7f050000;
-        public static final int login_button=0x7f050002;
-        public static final int pass_text=0x7f050001;
+        public static final int bid_item_bid_text=0x7f050005;
+        public static final int bid_item_text_id=0x7f050004;
+        public static final int bid_row_item_text=0x7f050003;
+        public static final int button1=0x7f050008;
+        public static final int button2=0x7f05000e;
+        public static final int buyout_price_edittext=0x7f050006;
+        public static final int editText1=0x7f050001;
+        public static final int email_text=0x7f050009;
+        public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f05000c;
+        public static final int linearLayout3=0x7f05000d;
+        public static final int listView1=0x7f050002;
+        public static final int login_button=0x7f05000b;
+        public static final int pass_text=0x7f05000a;
+        public static final int starting_price_edittext=0x7f050007;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
+        public static final int bidlist=0x7f030000;
+        public static final int bidlist_row=0x7f030001;
+        public static final int create_auction=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int profile=0x7f030004;
     }
     public static final class string {
+        public static final int add_item_button_text=0x7f040004;
         public static final int app_name=0x7f040001;
+        public static final int bid_item_screen_button=0x7f040005;
         public static final int hello=0x7f040000;
+        public static final int login_button_text=0x7f040002;
+        public static final int pick_button_date_text=0x7f040006;
+        public static final int search_button_text=0x7f040003;
     }
 }
