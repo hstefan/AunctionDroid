e5c7ae1cc4f11499f928d236afc59069b4b947c6
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-07-05 09:58:44 -0300

    Implementing User class.

--- src/com/hstefan/aunctiondroid/db/entities/User.java
+++ src/com/hstefan/aunctiondroid/db/entities/User.java
@@ -1,5 +1,34 @@
 package com.hstefan.aunctiondroid.db.entities;
 
 public class User {
+	private int id;
+	private String email;
+	private String pass;
+	
+	public User(int id, String email, String pass) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.pass = pass;
+	}
+	
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
 }
