184e77dd2b6cce452224f6efac4c9cf0eb3e9a39
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-07-06 17:39:10 -0300

    Adding a simple lockfile in order to stop inserting the same data on the table.

--- src/com/hstefan/aunctiondroid/db/DbHelper.java
+++ src/com/hstefan/aunctiondroid/db/DbHelper.java
@@ -1,5 +1,10 @@
 package com.hstefan.aunctiondroid.db;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
@@ -16,10 +21,32 @@ public class DbHelper extends SQLiteOpenHelper {
 	public static final String ITEM_TABLE = "item";
 	public static final String USER_ITEM_TABLE = "user_item";
 	
+	public static final String DB_SAMPLE_DATA_FILE = ".auctiondroid_sample";
+	
 	public DbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		getWritableDatabase().delete(USER_TABLE, null, null);
-		insertExampleData(getWritableDatabase());
+		//eraseTableRows();
+		try {
+			context.openFileInput(DB_SAMPLE_DATA_FILE);
+		} catch (FileNotFoundException e) {
+			try {
+				context.openFileOutput(DB_SAMPLE_DATA_FILE, 0);
+				insertExampleData(getWritableDatabase());
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	private void eraseTableRows() {
+		SQLiteDatabase db = getWritableDatabase();
+		db.delete(USER_ITEM_TABLE, null, null);
+		Log.i("Dropping table", USER_ITEM_TABLE);
+		db.delete(ITEM_TABLE, null, null);
+		Log.i("Dropping table", ITEM_TABLE);
+		db.delete(USER_TABLE, null, null);
+		Log.i("Dropping table", USER_TABLE);
 	}
 
 	@Override
@@ -30,24 +57,40 @@ public class DbHelper extends SQLiteOpenHelper {
 					"email TEXT NOT NULL," +
 					"password TEXT NOT NULL)";
 			db.execSQL(create_sql);
+			Log.i("Creating SQLite table", USER_TABLE);
 		} 
 		if(DATABASE_VERSION >= 2) {
 			String create_sql = "CREATE TABLE " + ITEM_TABLE +
 			"(id INTEGER PRIMARY KEY AUTOINCREMENT," +
 			"name TEXT NOT NULL)";
 			db.execSQL(create_sql);
+			Log.i("Creating SQLite table", ITEM_TABLE);
 			
 			create_sql = "CREATE TABLE " + USER_ITEM_TABLE +
 			"(id_user INTEGER, id_item INTEGER, " +
 			"FOREIGN KEY(id_user) REFERENCES " + USER_TABLE + ","  + 
 			"FOREIGN KEY(id_item) REFERENCES " + ITEM_TABLE + ")";
 			db.execSQL(create_sql);
+			Log.i("Creating SQLite table", USER_ITEM_TABLE);
 		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		
+		if(oldVersion == 1 && newVersion == 2) {
+			String create_sql = "CREATE TABLE " + ITEM_TABLE +
+			"(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+			"name TEXT NOT NULL)";
+			db.execSQL(create_sql);
+			Log.i("Creating SQLite table(upgrade)", ITEM_TABLE);
+			
+			create_sql = "CREATE TABLE " + USER_ITEM_TABLE +
+			"(id_user INTEGER, id_item INTEGER, " +
+			"FOREIGN KEY(id_user) REFERENCES " + USER_TABLE + ","  + 
+			"FOREIGN KEY(id_item) REFERENCES " + ITEM_TABLE + ")";
+			db.execSQL(create_sql);
+			Log.i("Creating SQLite table(upgrade)", USER_ITEM_TABLE);
+		}
 	}
 	
 	private void insertExampleData(SQLiteDatabase db) {
@@ -62,6 +105,6 @@ public class DbHelper extends SQLiteOpenHelper {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
+		} 
 	}
 }
