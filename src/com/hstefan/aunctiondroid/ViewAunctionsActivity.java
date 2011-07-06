a3d39e4d44f9c3627339ef8aa9aff3cd38e766ec
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-07-06 20:51:26 -0300

    Adding activity to view every aunction created.

---
+++ src/com/hstefan/aunctiondroid/ViewAunctionsActivity.java
@@ -0,0 +1,12 @@
+package com.hstefan.aunctiondroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewAunctionsActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		
+	}
+}
